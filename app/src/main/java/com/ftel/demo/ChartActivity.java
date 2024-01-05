abb062b33beb87101e1aef7cadf4f47d1e0f4b7a
nghiabanhmi <tthanhnghia1411@gmail.com>
2024-01-05 11:28:54 +0700

    update code 4

--- app/src/main/java/com/ftel/demo/ChartActivity.java
+++ app/src/main/java/com/ftel/demo/ChartActivity.java
@@ -13,6 +13,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -28,6 +29,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
@@ -41,14 +43,13 @@ import java.util.stream.Collectors;
 public class ChartActivity extends AppCompatActivity implements OnChartValueSelectedListener {
     private BarChart barChart;
     private RadioButton radioButton2_4GHz, radioButton5GHz;
+
+    private RadioGroup radioGroup;
     private WifiManager wifiManager;
     private Handler handler = new Handler(Looper.getMainLooper());
     private int temp = 0;
-    private boolean booleanCondition = false;
     private ArrayList<ScanResult> scanResults = new ArrayList<>();
-    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMMM, HH:mm:ss.SSS");
-
-    private boolean sortCondition = true;
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMMM, HH:mm:ss");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,23 +65,17 @@ public class ChartActivity extends AppCompatActivity implements OnChartValueSele
         barChart.getDescription().setEnabled(false);
         radioButton2_4GHz = findViewById(R.id.radio_2_4GHz);
         radioButton5GHz = findViewById(R.id.radio_5GHz);
+        radioGroup = findViewById(R.id.radioGroup);
+
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
-        radioButton2_4GHz.setOnClickListener(new View.OnClickListener() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "2.4GHz", Toast.LENGTH_SHORT).show();
-                sortCondition = true;
-                //startAndShowWifiResults(sortCondition);
-            }
-        });
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                handler.removeCallbacksAndMessages(null);
+                temp = 0;
+                startAndShowWifiResults(checkedId == radioButton2_4GHz.getId());
 
-        radioButton5GHz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "5GHz", Toast.LENGTH_SHORT).show();
-                sortCondition = false;
-                //startAndShowWifiResults(sortCondition);
             }
         });
     }
@@ -88,7 +83,7 @@ public class ChartActivity extends AppCompatActivity implements OnChartValueSele
     @Override
     protected void onResume() {
         super.onResume();
-        startAndShowWifiResults(sortCondition);
+        startAndShowWifiResults(true);
     }
 
     @Override
@@ -102,59 +97,64 @@ public class ChartActivity extends AppCompatActivity implements OnChartValueSele
             @Override
             public void run() {
                 if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    List<ScanResult> filteredScanResults = getScanWifiResults(is2_4GHz);
-
-                    ArrayList<BarEntry> data = filteredScanResults.stream()
-                            .map(result -> new BarEntry(filteredScanResults.indexOf(result),
-                                    (float) result.level,
-                                    TextUtils.isEmpty(result.SSID) ? result.BSSID : result.SSID))
-                            .collect(Collectors.toCollection(ArrayList::new));
-
-                    BarDataSet barDataSet = new BarDataSet(data, "Count: " + temp + " - Last Scan: " + dateFormatter.format(LocalDateTime.now()));
-                    temp++;
-                    barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-
-                    BarData barData = new BarData(barDataSet);
-
-                    barChart.setData(barData);
-                    barChart.invalidate();
-
-                    setupChart(filteredScanResults);
-                    booleanCondition = true;
+                    setDataChart(is2_4GHz);
+                    setupChart(is2_4GHz);
                 }
-
                 handler.postDelayed(this, 5000);
             }
         }, 0);
     }
 
-
     private List<ScanResult> getScanWifiResults(boolean is2_4GHz) {
+        if (temp != 0 && wifiManager.startScan()) {
+            temp++;
+        } else if (temp == 0) {
+            temp++;
+        }
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return new ArrayList<>();
         }
-
         return wifiManager.getScanResults().stream()
-                .filter(result -> result.level > -80)
+                .filter(result -> result.level > -100)
                 .filter(item -> is2_4GHz ?
                         wifiFrequencyToChannel(item.frequency) >= 1 && wifiFrequencyToChannel(item.frequency) <= 14 :
                         wifiFrequencyToChannel(item.frequency) >= 36 && wifiFrequencyToChannel(item.frequency) <= 165)
                 .collect(Collectors.toList());
     }
 
-    private void setupChart(List<ScanResult> filteredScanResults) {
+    private void setupChart(boolean is2_4GHz) {
         XAxis xAxis = barChart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setCenterAxisLabels(true);
+        xAxis.setCenterAxisLabels(false);
         xAxis.setGranularityEnabled(true);
         xAxis.setGranularity(1f);
-
         YAxis yAxis = barChart.getAxisLeft();
         yAxis.setInverted(true);
 
         barChart.getAxisRight().setEnabled(false);
 
         barChart.getXAxis().setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                int channel = Math.round(value);
+
+                if (is2_4GHz) {
+                    if (channel >= 1 && channel <= 14) {
+                        return String.valueOf(channel);
+                    }
+                } else {
+                    if (channel >= 36 && channel <= 165) {
+                        return String.valueOf(channel);
+                    }
+                }
+
+                return "";
+            }
+        });
+
+
+        /*barChart.getXAxis().setValueFormatter(new ValueFormatter() {
             @Override
             public String getFormattedValue(float value) {
                 int index = (int) value;
@@ -164,38 +164,61 @@ public class ChartActivity extends AppCompatActivity implements OnChartValueSele
                 return "N/A";
             }
         });
-    }
-
-    private int wifiFrequencyToChannel(int frequency) {
-        if (frequency >= 2412 && frequency <= 2484) {
-            return (frequency - 2412) / 5 + 1;
-        } else if (frequency >= 5170 && frequency <= 5825) {
-            return (frequency - 5170) / 5 + 34;
-        } else {
-            return -1;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            startAndShowWifiResults(false);
-        }
-    }
-
-    @Override
-    public void onValueSelected(Entry e, Highlight h) {
-
-    }
-
-    @Override
-    public void onNothingSelected() {
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
-    }
+    }*/
 }
+
+        private void setDataChart (boolean is2_4GHz) {
+            List<ScanResult> filteredScanResults = getScanWifiResults(is2_4GHz);
+            ArrayList<String> xValues = new ArrayList<>();
+            ArrayList<BarEntry> yValues = new ArrayList<>();
+            filteredScanResults.forEach(i -> {
+                String label = TextUtils.isEmpty(i.SSID) ? i.BSSID : i.SSID;
+
+                BarEntry barEntry = new BarEntry(Float.valueOf(wifiFrequencyToChannel(i.frequency)), filteredScanResults.indexOf(i));
+                //xValues.add(label);
+                yValues.add(barEntry);
+            });
+
+
+            BarDataSet barDataSet = new BarDataSet(yValues, "Count: " + (temp) + " - Last Scan: " + dateFormatter.format(LocalDateTime.now()));
+            BarData barData = new BarData(barDataSet);
+            barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+            barChart.setData(barData);
+            barChart.invalidate();
+        }
+
+        private int wifiFrequencyToChannel ( int frequency){
+            if (frequency >= 2412 && frequency <= 2484) {
+                return (frequency - 2412) / 5 + 1;
+            } else if (frequency >= 5170 && frequency <= 5825) {
+                return (frequency - 5170) / 5 + 34;
+            } else {
+                return -1;
+            }
+        }
+
+        @Override
+        public void onRequestPermissionsResult ( int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults){
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startAndShowWifiResults(false);
+            }
+        }
+
+        @Override
+        public void onValueSelected (Entry e, Highlight h){
+
+        }
+
+        @Override
+        public void onNothingSelected () {
+
+        }
+
+        @Override
+        public void onPointerCaptureChanged ( boolean hasCapture){
+            super.onPointerCaptureChanged(hasCapture);
+        }
+    }
